7bf0e4ecb856b6835f733de2937f4334367b63c3
pauloalvesm <paulo.alves7351@gmail.com>
2026-01-23 10:09:27 -0300

    feat: create the home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+            <h1 className="text-4xl font-bold text-blue-700 mb-4">Welcome to QR Code Generator</h1>
+            <p className="text-gray-600 text-lg">Use the menu to navigate.</p>
+        </div>
+    );
+}
