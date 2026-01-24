c0da73d7f63ceedb6cdda198e74701510c7b2dcb
pauloalvesm <paulo.alves7351@gmail.com>
2026-01-24 10:18:34 -0300

    feat: define the components to generate the qr code

---
+++ src/components/QrCode/QrCodeViewer.tsx
@@ -0,0 +1,21 @@
+interface Props {
+    imageBase64: string;
+}
+
+export default function QrCodeViewer({ imageBase64 }: Props) {
+    return (
+        <>
+            <img 
+                src={imageBase64} 
+                alt="QR Code" 
+                className="max-w-[300px] object-contain rounded-lg border border-gray-300 shadow-md my-2" 
+            />
+            
+            <a href={imageBase64} download="qrcode.png">
+                <button className="mt-4 px-6 py-2 bg-green-600 text-white font-semibold rounded-lg shadow hover:bg-green-700 transition">
+                    Download QR Code
+                </button>
+            </a>
+        </>
+    )
+}
