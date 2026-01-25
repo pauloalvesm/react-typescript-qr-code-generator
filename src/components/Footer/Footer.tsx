c173cd97c13c23b4f320690138f84d8472ebdb57
pauloalvesm <paulo.alves7351@gmail.com>
2026-01-25 14:00:30 -0300

    feat: define method to display current year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="bg-gray-100 text-center py-4 border-t border-gray-300 mt-auto">
             <p className="text-sm text-gray-600">
-                <span className="font-semibold">2026 - Developed by:</span>{" "}
+                <span className="font-semibold">{ new Date().getFullYear() } - Developed by:</span>{" "}
                 <a 
                     href="https://github.com/pauloalvesm"
                     target="_blank"
